72526b8a837c8ee93f3ab64d9d60aa119dcb0c35
ZZZank <3410764033@qq.com>
2024-12-05 23:29:17 +0800

    field wrapper noted

--- common/src/main/java/dev/latvian/mods/rhino/native_java/JField.java
+++ common/src/main/java/dev/latvian/mods/rhino/native_java/JField.java
@@ -1,25 +1,31 @@
 package dev.latvian.mods.rhino.native_java;
 
 import dev.latvian.mods.rhino.Context;
-import lombok.AllArgsConstructor;
+import dev.latvian.mods.rhino.native_java.type.info.TypeInfo;
+import lombok.val;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 /**
  * @author ZZZank
  */
-@AllArgsConstructor
 public class JField {
-    public final String nativeName;
+    public final Field raw;
     public final String remappedName;
     public final int index;
 
-    public final Class<?> type;
+    public final TypeInfo type;
+    public final boolean isStatic;
+    public final boolean isFinal;
 
     public JField(Field f, Class<?> from, int index, Context cx) {
-        nativeName = f.getName();
+        raw = f;
         remappedName = cx.getRemapper().remapField(from, f);
         this.index = index;
-        type = f.getType();
+        this.type = TypeInfo.of(f.getGenericType());
+        val mod = f.getModifiers();
+        this.isStatic = Modifier.isStatic(mod);
+        this.isFinal = Modifier.isFinal(mod);
     }
 }
