4eae9240ede316eaf8041e76efa1b8c99ba3d949
ZZZank <3410764033@qq.com>
2024-12-06 10:39:23 +0800

    use MethodHandle instead of ReflectASM for accessing field

--- common/src/main/java/dev/latvian/mods/rhino/native_java/JField.java
+++ common/src/main/java/dev/latvian/mods/rhino/native_java/JField.java
@@ -1,29 +1,34 @@
 package dev.latvian.mods.rhino.native_java;
 
+import dev.latvian.mods.rhino.Context;
 import dev.latvian.mods.rhino.VMBridge;
-import dev.latvian.mods.rhino.native_java.reflectasm.FieldAccess;
 import dev.latvian.mods.rhino.native_java.type.info.TypeInfo;
 import lombok.val;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.IdentityHashMap;
-import java.util.Map;
 
 /**
  * @author ZZZank
  */
 public class JField {
-    private static final Map<Class<?>, FieldAccess> ACCESSES = new IdentityHashMap<>();
+
+    private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
+    private static final byte ACCESS_GETTER = 1;
+    private static final byte ACCESS_SETTER = ACCESS_GETTER << 1;
 
     public final Field raw;
     public final String name;
-    public final int index;
 
     public final TypeInfo type;
     public final boolean isStatic;
     public final boolean isFinal;
-    private final FieldAccess access;
+
+    private byte access = 0;
+    private MethodHandle getter;
+    private MethodHandle setter;
 
     public JField(Field f, Class<?> from, String name) {
         this.raw = f;
@@ -32,25 +37,65 @@ public class JField {
         val mod = f.getModifiers();
         this.isStatic = Modifier.isStatic(mod);
         this.isFinal = Modifier.isFinal(mod);
-        this.access = ACCESSES.computeIfAbsent(from, FieldAccess::get);
-        this.index = access.getIndex(this.raw);
     }
 
     public void set(Object instance, Object value) {
         try {
-            access.set(instance, index, value);
-        } catch (IllegalAccessError e) {
-            VMBridge.vm.tryToMakeAccessible(raw);
-            access.set(instance, index, value);
+            setInternal(instance, value);
+        } catch (IllegalAccessException e) {
+            val accessible = VMBridge.vm.tryToMakeAccessible(raw);
+            if (!accessible) {
+                throw Context.throwAsScriptRuntimeEx(e);
+            }
+            try { // retry after recovery
+                setInternal(instance, value);
+            } catch (Throwable ex) {
+                throw Context.throwAsScriptRuntimeEx(ex);
+            }
+        } catch (Throwable e) {
+            throw Context.throwAsScriptRuntimeEx(e);
+        }
+    }
+
+    private void setInternal(Object instance, Object value) throws IllegalAccessException, Throwable {
+        if ((access & ACCESS_SETTER) == 0) {
+            access = (byte) (access | ACCESS_SETTER);
+            setter = LOOKUP.unreflectSetter(raw);
+        }
+        if (isStatic) {
+            setter.invoke(value);
+        } else {
+            setter.invoke(instance, value);
+        }
+    }
+
+    private Object setInternal(Object instance) throws IllegalAccessException, Throwable {
+        if ((access & ACCESS_GETTER) == 0) {
+            access = (byte) (access | ACCESS_GETTER);
+            getter = LOOKUP.unreflectGetter(raw);
+        }
+        if (isStatic) {
+            return getter.invoke();
+        } else {
+            return getter.invoke(instance);
         }
     }
 
     public Object get(Object instance) {
         try {
-            return access.get(instance, index);
-        } catch (IllegalAccessError e) {
-            VMBridge.vm.tryToMakeAccessible(raw);
-            return access.get(instance, index);
+            return setInternal(instance);
+        } catch (IllegalAccessException e) {
+            val accessible = VMBridge.vm.tryToMakeAccessible(raw);
+            if (!accessible) {
+                throw Context.throwAsScriptRuntimeEx(e);
+            }
+            try { // retry after recovery
+                return setInternal(instance);
+            } catch (Throwable ex) {
+                throw Context.throwAsScriptRuntimeEx(ex);
+            }
+        } catch (Throwable e) {
+            throw Context.throwAsScriptRuntimeEx(e);
         }
     }
 }
