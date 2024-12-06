8bb365420fd23e2631b3e6099b59fe7d88e45c51
ZZZank <3410764033@qq.com>
2024-12-06 13:22:53 +0800

    use ReentrantReadWriteLock instead

--- common/src/main/java/dev/latvian/mods/rhino/native_java/type/info/BasicClassTypeInfo.java
+++ common/src/main/java/dev/latvian/mods/rhino/native_java/type/info/BasicClassTypeInfo.java
@@ -1,20 +1,33 @@
 package dev.latvian.mods.rhino.native_java.type.info;
 
+import lombok.val;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.IdentityHashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class BasicClassTypeInfo extends ClassTypeInfo {
 	static final Map<Class<?>, BasicClassTypeInfo> CACHE = new IdentityHashMap<>();
+	private static final Lock WRITE_LOCK;
+	private static final Lock READ_LOCK;
+
+	static {
+		val l = new ReentrantReadWriteLock();
+		READ_LOCK = l.readLock();
+		WRITE_LOCK = l.writeLock();
+	}
 
 	static @NotNull BasicClassTypeInfo of(Class<?> c) {
+		READ_LOCK.lock();
 		var got = CACHE.get(c);
+		READ_LOCK.unlock();
 		if (got == null) {
-			synchronized (CACHE) {
-				got = new BasicClassTypeInfo(c);
-				CACHE.put(c, got);
-			}
+			WRITE_LOCK.lock();
+			got = new BasicClassTypeInfo(c);
+			CACHE.put(c, got);
+			WRITE_LOCK.unlock();
 		}
 		return got;
 	}
--- common/src/main/java/dev/latvian/mods/rhino/native_java/type/info/InterfaceTypeInfo.java
+++ common/src/main/java/dev/latvian/mods/rhino/native_java/type/info/InterfaceTypeInfo.java
@@ -1,18 +1,31 @@
 package dev.latvian.mods.rhino.native_java.type.info;
 
+import lombok.val;
+
 import java.util.IdentityHashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class InterfaceTypeInfo extends ClassTypeInfo {
 	static final Map<Class<?>, InterfaceTypeInfo> CACHE = new IdentityHashMap<>();
+	private static final Lock WRITE_LOCK;
+	private static final Lock READ_LOCK;
+
+	static {
+		val l = new ReentrantReadWriteLock();
+		READ_LOCK = l.readLock();
+		WRITE_LOCK = l.writeLock();
+	}
 
 	static InterfaceTypeInfo of(Class<?> c) {
+		READ_LOCK.lock();
 		var got = CACHE.get(c);
+		READ_LOCK.unlock();
 		if (got == null) {
-			synchronized (CACHE) {
-				got = new InterfaceTypeInfo(c, null);
-				CACHE.put(c, got);
-			}
+			WRITE_LOCK.lock();
+			CACHE.put(c, got = new InterfaceTypeInfo(c, null));
+			WRITE_LOCK.unlock();
 		}
 		return got;
 	}
