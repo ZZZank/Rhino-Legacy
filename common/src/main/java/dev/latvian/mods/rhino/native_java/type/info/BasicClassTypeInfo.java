cdf578758b6a728f14a95ac2197f031ab6589284
ZZZank <3410764033@qq.com>
2024-12-06 13:12:25 +0800

    fine-grained concurrent factory method for BasicClassTypeInfo

--- common/src/main/java/dev/latvian/mods/rhino/native_java/type/info/BasicClassTypeInfo.java
+++ common/src/main/java/dev/latvian/mods/rhino/native_java/type/info/BasicClassTypeInfo.java
@@ -1,10 +1,23 @@
 package dev.latvian.mods.rhino.native_java.type.info;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class BasicClassTypeInfo extends ClassTypeInfo {
 	static final Map<Class<?>, BasicClassTypeInfo> CACHE = new IdentityHashMap<>();
+
+	static @NotNull BasicClassTypeInfo of(Class<?> c) {
+		var got = CACHE.get(c);
+		if (got == null) {
+			synchronized (CACHE) {
+				got = new BasicClassTypeInfo(c);
+				CACHE.put(c, got);
+			}
+		}
+		return got;
+	}
 
 	BasicClassTypeInfo(Class<?> type) {
 		super(type);
