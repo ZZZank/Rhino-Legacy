86933440934ca5e5ff653a2de6cfebda3570eb65
ZZZank <3410764033@qq.com>
2024-12-06 11:51:50 +0800

    perf test

--- _dev/graph/gr.py
+++ _dev/graph/gr.py
@@ -3,23 +3,27 @@
 import csv
 from matplotlib import pyplot as plt
 
-opens = [
+opens: list[tuple[str, str]] = [
     ['rhino.txt','r'],
     ['rhino_2nd_run.txt','orange'],
     # 'rhizo.txt',
     ['rhizo_compile.txt','g'],
-    ['rhizo_compile_2nd_run.txt','blue']
+    ['rhizo_compile_2nd_run.txt','blue'],
+    ['rhizo3.4.txt','y']
 ]
 
 legends = []
 
 for name, fmt in opens:
+    lines: list[str] = []
     with open(name) as f:
-        reader = csv.DictReader(f)
-        duras = [int(l['duration']) for l in reader]
-        plt.plot(range(len(duras)), duras, fmt)
-        legends.append(f'{name.split(".", 1)[0]}: {sum(duras) / len(duras)}ms')
-        pass
+        lines = [line for line in f.readlines() if not line.startswith('#')]
+    reader = csv.DictReader(lines)
+    duras = [int(l['duration']) for l in reader]
+    plt.plot(range(len(duras)), duras, fmt)
+    fileName = '.'.join(name.split('.')[:-1])
+    legends.append(f'{fileName}: {sum(duras) / len(duras)}ms')
+    pass
 
 plt.legend(legends)
 plt.axis((-5, 165, 100, 600))
