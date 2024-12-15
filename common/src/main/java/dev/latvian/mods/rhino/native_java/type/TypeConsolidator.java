7f41dd62102b0215d45dae27e20756e1b362369d
ZZZank <3410764033@qq.com>
2024-12-15 15:33:14 +0800

    fix type consolidating, add some shortcut

--- common/src/main/java/dev/latvian/mods/rhino/native_java/type/TypeConsolidator.java
+++ common/src/main/java/dev/latvian/mods/rhino/native_java/type/TypeConsolidator.java
@@ -7,6 +7,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.ParameterizedType;
 import java.util.*;
@@ -18,12 +19,18 @@ import java.util.*;
 public final class TypeConsolidator {
     private static final Map<Class<?>, Map<VariableTypeInfo, TypeInfo>> MAPPINGS = new Reference2ObjectOpenHashMap<>();
 
+    private static final boolean DEBUG = false;
+
     @NotNull
     public static Map<VariableTypeInfo, TypeInfo> getMapping(Class<?> type) {
+        if (DEBUG) {
+            System.out.println("getting mapping from: " + type);
+        }
         val got = getImpl(type);
         return got == null ? Collections.emptyMap() : got;
     }
 
+    @NotNull
     public static TypeInfo consolidateOrNone(VariableTypeInfo variable, Map<VariableTypeInfo, TypeInfo> mapping) {
         return mapping.getOrDefault(variable, TypeInfo.NONE);
     }
@@ -33,17 +40,26 @@ public final class TypeConsolidator {
         @NotNull TypeInfo @NotNull [] original,
         @NotNull Map<VariableTypeInfo, TypeInfo> mapping
     ) {
+        val len = original.length;
+        if (DEBUG) {
+            System.out.println("consolidating" + Arrays.toString(original));
+        }
+        if (len == 0) {
+            return original;
+        } else if (len == 1) {
+            val consolidated = original[0].consolidate(mapping);
+            return consolidated != original[0] ? new TypeInfo[]{consolidated} : original;
+        }
         TypeInfo[] consolidatedAll = null;
-        for (int i = 0; i < original.length; i++) {
+        for (int i = 0; i < len; i++) {
             val type = original[i];
             val consolidated = type.consolidate(mapping);
             if (consolidated != type) {
                 if (consolidatedAll == null) {
-                    consolidatedAll = new TypeInfo[original.length];
+                    consolidatedAll = new TypeInfo[len];
                     System.arraycopy(original, 0, consolidatedAll, 0, i);
-                } else {
-                    consolidatedAll[i] = consolidated;
                 }
+                consolidatedAll[i] = consolidated;
             } else if (consolidatedAll != null) {
                 consolidatedAll[i] = consolidated;
             }
@@ -51,6 +67,7 @@ public final class TypeConsolidator {
         return consolidatedAll == null ? original : consolidatedAll;
     }
 
+    @Nullable
     private static Map<VariableTypeInfo, TypeInfo> getImpl(Class<?> type) {
         if (type == null || type.isPrimitive() || type == Object.class) {
             return null;
@@ -58,14 +75,13 @@ public final class TypeConsolidator {
         var got = MAPPINGS.get(type);
         if (got == null) {
             synchronized (MAPPINGS) {
-                if (got == null) {
-                    MAPPINGS.put(type, got = collect(type));
-                }
+                got = MAPPINGS.computeIfAbsent(type, TypeConsolidator::collect);
             }
         }
         return got;
     }
 
+    @NotNull
     private static Map<VariableTypeInfo, TypeInfo> collect(Class<?> type) {
         val mapping = new IdentityHashMap<VariableTypeInfo, TypeInfo>();
 
@@ -86,17 +102,25 @@ public final class TypeConsolidator {
         //mapping from super
         val superMapping = getImpl(parent);
         if (superMapping == null) {
-            return Collections.unmodifiableMap(mapping);
+            return postMapping(mapping);
         }
         val merged = new IdentityHashMap<VariableTypeInfo, TypeInfo>();
         for (val entry : superMapping.entrySet()) {
             merged.put(entry.getKey(), entry.getValue().consolidate(mapping));
         }
         merged.putAll(mapping);
+        return postMapping(merged);
+    }
 
-        if (merged.isEmpty()) {
-            return Collections.emptyMap();
+    private static Map<VariableTypeInfo, TypeInfo> postMapping(Map<VariableTypeInfo, TypeInfo> mapping) {
+        switch (mapping.size()) {
+            case 0:
+                return Collections.emptyMap();
+            case 1:
+                val entry = mapping.entrySet().iterator().next();
+                return Collections.singletonMap(entry.getKey(), entry.getValue());
+            default:
+                return Collections.unmodifiableMap(mapping);
         }
-        return Collections.unmodifiableMap(merged);
     }
 }
